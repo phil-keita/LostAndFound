ae96ad617a582cec172970228dbff63a9f7474dd
Robo-Eye <68978948+Robo-Eye@users.noreply.github.com>
2023-12-12 22:08:39 -0500

    chat screen on click

--- app/src/main/java/com/example/lostandfound/screens/ChatScreen.kt
+++ app/src/main/java/com/example/lostandfound/screens/ChatScreen.kt
@@ -9,10 +9,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -29,32 +27,24 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
 import com.example.lostandfound.LafViewModel
 import com.example.lostandfound.model.LAFMessage
 import com.example.lostandfound.model.Conversation
-import com.example.lostandfound.presentation.sign_in.UserData
-import com.example.lostandfound.ui.theme.md_theme_dark_onTertiaryContainer
 import com.example.lostandfound.ui.theme.md_theme_dark_secondaryContainer
 import com.example.lostandfound.ui.theme.md_theme_light_onPrimary
 import com.example.lostandfound.ui.theme.md_theme_light_onPrimaryContainer
@@ -69,6 +59,7 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.LiveData
 
 import com.example.lostandfound.model.LostPost
 import com.example.lostandfound.ui.theme.md_theme_light_onTertiary
@@ -215,6 +206,9 @@ fun conversations(VM: LafViewModel){
 //    val conversations: List<Map<String, Any>> by VM.messages.observeAsState(
 //            initial = emptyList<Map<String, Any>>().toMutableList()
 //            )
+    var notEmptyChat by remember {
+        mutableStateOf(false)
+    }
 
     val conversations by VM.conversations.observeAsState(initial = mutableListOf())
     val conversation by VM.conversation.observeAsState(initial = emptyMap())
@@ -263,6 +257,11 @@ fun conversations(VM: LafViewModel){
                 ) {
                     items(conversations) { conversation ->
 
+                        Card(onClick = {
+                            if (conversation.isNotEmpty()){
+                                notEmptyChat = true //chat(vm,conversation)
+                            }
+                        }) {
                         Card {
                             Column(
                                 modifier = Modifier
@@ -278,9 +277,6 @@ fun conversations(VM: LafViewModel){
                     }
                 }
             }
-        }
-        if (conversation.isNotEmpty()){
-            Chat(VM = VM, conversation = conversation)
         }
     }
 }
